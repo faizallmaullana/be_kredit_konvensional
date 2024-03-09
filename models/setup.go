f6366251ce250340ef3ae40203545d62f35b1090
faizallmaullana <faizalmaulanasekias@gmail.com>
2024-03-09 10:24:03 +0700

    test: for build

--- models/setup.go
+++ models/setup.go
@@ -12,10 +12,10 @@ import (
 var DB *gorm.DB
 
 func ConnectToDatabase() {
-	// connStr := "user='tanjung' password=APWa4n3XiTfx host=ep-green-wave-a1c4bn3h.ap-southeast-1.pg.koyeb.app dbname='koyebdb'"
-	// database, err := gorm.Open("postgres", connStr)
+	connStr := "user='tanjung' password=APWa4n3XiTfx host=ep-green-wave-a1c4bn3h.ap-southeast-1.pg.koyeb.app dbname='koyebdb'"
+	database, err := gorm.Open("postgres", connStr)
 
-	database, err := gorm.Open("sqlite3", "database.db")
+	// database, err := gorm.Open("sqlite3", "database.db")
 
 	if err != nil {
 		panic("Failed to connect to database!")
